ef4ccbf8d369e764cfd2e90e7f5857495e09df4e
DanielSaldivar1 <85500359+DanielSaldivar1@users.noreply.github.com>
2021-06-14 08:24:51 -0500

    Add files via upload

---
+++ DanielsMusic.js
@@ -0,0 +1,10 @@
+function NextFunction()
+{
+	console.log("NEXTclicked");
+	window.location.href = "DanielsMusic.html";
+
+function BackFunction()
+{
+	console.log("Backclicked");
+	window.location.href = "DanielsPetsShop.html";
+}
